247be74e63fa5937828a52d5e246dd0f50c1c687
Tom George <tomgeorge1@btinternet.com>
2021-04-21 00:05:28 +0100

    more experiments

--- mazeAgentUtils.py
+++ mazeAgentUtils.py
@@ -32,7 +32,7 @@ rcParams['axes.prop_cycle']=cycler('color', ['#66c2a5','#fc8d62','#8da0cb','#e78
 
 
 class MazeAgent():
-    def __init__(self,dt=0.01,numRooms=2, roomSize=1, policy='randomWalk', state='onehot', doorsClosed=False, dx=0.1, v=1):
+    def __init__(self, dt=0.01, numRooms=1, roomSize=1, policy='randomWalk', state='onehot', doorsClosed=False, dx=0.1, v=1):
 
         #arguments
         self.numRooms = numRooms
@@ -40,12 +40,13 @@ class MazeAgent():
         self.dt = dt
         self.dx = dx
         self.v = v
-        self.policy = policy
-        self.state = state
+        self.policy = policy 
+        self.stateType = state
 
         #SR learning params
-        self.gamma = 0.95
-        self.alpha = 0.2
+        self.gamma = 0.999
+        self.alpha = 0.3
+
         #initialise state
         self.pos = np.array([0.2,0.2])
         self.prevPos = np.array([0.2,0.2])
@@ -56,18 +57,24 @@ class MazeAgent():
         #some attributes
         self.posHist = np.array([self.pos])
         self.walls = {}
-        self.makeWalls()
-        self.initStateVector()
+        self.initialiseMaze()
 
-    def makeWalls(self):
+    def initialiseMaze(self):
         assert self.numRooms in [1,2,4]
         rs = self.roomSize
+        dx = self.dx
+
         if self.numRooms == 1: 
             self.walls['room1'] = np.array([
                                     [[0,0],[0,rs]],
                                     [[0,rs],[rs,rs]],
                                     [[rs,rs],[rs,0]],
                                     [[rs,0],[0,0]]])
+
+            self.xArray = np.arange(0,rs,dx)
+            self.yArray = np.arange(0,rs,dx)[::-1]
+            self.extent = (0,rs,0,rs)
+
         elif self.numRooms == 2:
             self.walls['room1'] = np.array([
                                     [[0,0],[0,rs]],
@@ -82,6 +89,11 @@ class MazeAgent():
                                     [[2*rs,rs],[2*rs,0]],
                                     [[2*rs,0],[rs,0]]])
             self.walls['door12'] = np.array([[[rs,0.4*rs],[rs,0.6*rs]]])
+
+            self.xArray = np.arange(dx/2,2*rs,dx)
+            self.yArray = np.arange(dx/2,rs,dx)[::-1]
+            self.extent = (0,2*rs,0,rs)
+
         elif self.numRooms == 4: 
             self.walls['room1'] = np.array([
                                     [[0,0],[0,rs]],
@@ -116,6 +128,15 @@ class MazeAgent():
             self.walls['door34'] = np.array([[[rs,1.4*rs],[rs,1.6*rs]]])
             self.walls['door24'] = np.array([[[1.4*rs,rs],[1.6*rs,rs]]])
 
+            self.xArray = np.arange(dx/2,2*rs,dx)
+            self.yArray = np.arange(dx/2,2*rs,dx)[::-1]
+            self.extent = (0,2*rs,0,2*rs)
+
+        
+        self.stateVec_asMatrix = np.zeros(shape=(len(self.yArray),len(self.xArray)))
+        self.stateVec_asVector = self.stateVec_asMatrix.reshape((-1))
+        self.M = np.eye(len(self.stateVec_asVector))
+
     
     def movementPolicyUpdate(self):
         self.posHist = np.vstack((self.posHist,self.pos))
@@ -130,32 +151,28 @@ class MazeAgent():
             self.theta = np.mod(self.theta,2*np.pi)
             self.vel = self.v*np.array([np.cos(self.theta),np.sin(self.theta)])
 
-    def plotHistory(self,color=None):
-        fig, ax = plt.subplots(figsize=(2,2))
+    def plotMovementHistory(self,color=None):
+        fig, ax = self.plotMazeStructure()
         ax.scatter(self.posHist[:,0],self.posHist[:,1],s=0.1,alpha=0.5,c=color)
-        maxWall, minWall = [0,0],[0,0] 
-        for wallObject in self.walls.keys():
-            if (wallObject[:4] == 'door' and self.doorsClosed == False):
-                continue
-
-            for wall in self.walls[wallObject]:
-                ax.plot([wall[0][0],wall[1][0]],[wall[0][1],wall[1][1]],color='darkgrey',linewidth=2)
-                maxWall[0] = max(maxWall[0],max(wall[:,0]))
-                minWall[0] = min(minWall[0],min(wall[:,0]))
-                maxWall[1] = max(maxWall[1],max(wall[:,1]))
-                minWall[1] = min(minWall[1],min(wall[:,1]))
-            ax.set_ylim(bottom=minWall[1]-0.05,top=maxWall[1]+0.05)
-            ax.set_xlim(left=minWall[0]-0.05,right=maxWall[0]+0.05)
-        ax.set_aspect('equal')
         return fig, ax
 
-        
+    #def plotSuccessorFeatures(self, pos=None):
+    def plotStateVector(self, pos=None):
+        if self.stateType == 'onehot':
+            if pos is None: pos = self.pos
+            state = self.posToState(pos).reshape(self.stateVec_asMatrix.shape)
+            fig, ax = self.plotMazeStructure()
+            ax.imshow(state, extent=self.extent,cmap='viridis')
+        else:
+            state = self.posToState(pos)
+            plt.imshow(state)
+
+
     def plotPlaceField(self,number=None):
         if number == None: number = random.randint(a=0,b=len(self.stateVec_asVector)-1)
         placeField = self.M[:,number].reshape(self.stateVec_asMatrix.shape)
         fig, ax = plt.subplots(figsize=(2,2))
-
-        ax.imshow(placeField,extent=(0,1,0,1),cmap='viridis')
+        ax.imshow(placeField,extent=self.extent,cmap='viridis')
         maxWall, minWall = [0,0],[0,0] 
         for wallObject in self.walls.keys():
             if (wallObject[:4] == 'door' and self.doorsClosed == False):
@@ -169,16 +186,23 @@ class MazeAgent():
             ax.set_ylim(bottom=minWall[1]-0.05,top=maxWall[1]+0.05)
             ax.set_xlim(left=minWall[0]-0.05,right=maxWall[0]+0.05)
         ax.set_aspect('equal')
+        ax.grid(False)
         return fig, ax
-
-
+    
+    
     def plotGridField(self,number=None):
         if number == None: number = random.randint(a=0,b=len(self.stateVec_asVector)-1)
-        eigval, eigvec = np.linalg.eig(self.M)
+        _, eigvec = np.linalg.eig(self.M)
         eigvec = np.real(eigvec)
         gridField = eigvec[:,number].reshape(self.stateVec_asMatrix.shape)
         fig, ax = plt.subplots(figsize=(2,2))
-        ax.imshow(gridField,extent=(0,1,0,1),cmap='viridis')
+        if self.numRooms == 1: 
+            extent=(0,1,0,1)
+        elif self.numRooms == 2: 
+            extent=(0,2,0,1)
+        elif self.numRooms == 4: 
+            extent=(0,2,0,2)
+        ax.imshow(gridField,extent=extent,cmap='viridis')
         maxWall, minWall = [0,0],[0,0] 
         for wallObject in self.walls.keys():
             if (wallObject[:4] == 'door' and self.doorsClosed == False):
@@ -192,10 +216,8 @@ class MazeAgent():
             ax.set_ylim(bottom=minWall[1]-0.05,top=maxWall[1]+0.05)
             ax.set_xlim(left=minWall[0]-0.05,right=maxWall[0]+0.05)
         ax.set_aspect('equal')
+        ax.grid(False)
         return fig, ax
-
-
-
 
     def checkWallIntercepts(self,step):
         # calculates point of intercept between the line passing along the current step direction and the lines passing along the walls,
@@ -220,7 +242,6 @@ class MazeAgent():
                         earliestCollision = lam_s
                         self.wallBounce(wall)
         return earliestCollision - 0.01
-
         
     def wallBounce(self,wall):
         wallPerp = perp(wall[1] - wall[0])
@@ -229,20 +250,7 @@ class MazeAgent():
         wallTheta = theta(wallPerp)
         self.theta = np.mod(wallTheta + np.pi - (self.theta - wallTheta), 2*np.pi)
 
-    def initStateVector(self):
-        if self.state == 'onehot':
-            if self.numRooms == 1: 
-                self.xArray = np.arange(0,self.roomSize,self.dx)
-                self.yArray = np.arange(0,self.roomSize,self.dx)[::-1]
-            elif self.numRooms == 2: 
-                self.xArray = np.arange(0,2*self.roomSize,self.dx)
-                self.yArray = np.arange(0,self.roomSize,self.dx)[::-1]
-            elif self.numRooms == 4: 
-                self.xArray = np.arange(0,2*self.roomSize,self.dx)
-                self.yArray = np.arange(0,2*self.roomSize,self.dx)[::-1]
-            self.stateVec_asMatrix = np.zeros(shape=(len(self.yArray),len(self.xArray)))
-            self.stateVec_asVector = self.stateVec_asMatrix.reshape((-1))
-            self.M = np.eye(len(self.stateVec_asVector))
+
 
     def TDLearningStep(self):
         currentState = self.posToState(self.pos)
@@ -250,14 +258,34 @@ class MazeAgent():
         delta = prevState + self.gamma*np.matmul(self.M,currentState) - np.matmul(self.M,prevState)
         self.M = self.M + self.alpha * np.outer(delta, prevState)
 
+    def plotMazeStructure(self):
+        fig, ax = plt.subplots(figsize=(2,2))
+        maxWall, minWall = [0,0],[0,0] 
+        for wallObject in self.walls.keys():
+            if (wallObject[:4] == 'door' and self.doorsClosed == False):
+                continue
+            for wall in self.walls[wallObject]:
+                ax.plot([wall[0][0],wall[1][0]],[wall[0][1],wall[1][1]],color='darkgrey',linewidth=2)
+                maxWall[0] = max(maxWall[0],max(wall[:,0]))
+                minWall[0] = min(minWall[0],min(wall[:,0]))
+                maxWall[1] = max(maxWall[1],max(wall[:,1]))
+                minWall[1] = min(minWall[1],min(wall[:,1]))
+            ax.set_ylim(bottom=minWall[1]-0.05,top=maxWall[1]+0.05)
+            ax.set_xlim(left=minWall[0]-0.05,right=maxWall[0]+0.05)
+        ax.set_aspect('equal')
+        ax.grid(False)
 
-    def posToState(self,pos):
-        xid = np.argmin(np.abs(self.xArray - pos[0]))
-        yid = np.argmin(np.abs(self.yArray - pos[1]))
-        state = np.zeros_like(self.stateVec_asMatrix)
-        state[yid,xid] = 1
-        state = state.reshape(self.stateVec_asVector.shape)
+        return fig, ax
+
+    def posToState(self,pos,stateType='onehot'): #redo this?
+        if stateType == 'onehot':
+            xid = np.argmin(np.abs(self.xArray - pos[0]))
+            yid = np.argmin(np.abs(self.yArray - pos[1]))
+            state = np.zeros_like(self.stateVec_asMatrix)
+            state[yid,xid] = 1
+            state = state.reshape(self.stateVec_asVector.shape)
         return state
+
 
 
 
