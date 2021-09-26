29593bc441c6f99f2856f9ca71c696772fe06799
Tom George <tgeorge@sgw2.id.swc.ucl.ac.uk>
2021-09-26 11:39:52 +0000

    slurm eds

--- otherScripts/slurmMaker.py
+++ otherScripts/slurmMaker.py
@@ -1,16 +1,31 @@
+import subprocess
+subprocess.run("rm slurmScript.sh", shell=True)
 
 K = [0.5, 1, 2]
 T = [20e-2, 25e-3, 30e-3]
 A = [0.6, 0.7, 0.8]
 F = [0.6, 0.8, 1]
 
-with open("slurmDefault.sh","r") as default:
-    with open("slurmScript.sh","a") as new: 
-        for line in default:
-            new.write(line)
+pre_schpeel = [
+"#!/bin/bash \n",
+"#SBATCH --job-name=hpsweep              #name of the job to find when calling >>>sacct or >>>squeue \n",
+"#SBATCH --nodes=4                               #number of nodes, i.e. computers, to request off the cluster (nodes typically have ~20 singled threaded cores) \n",
+"#SBATCH --ntasks=%g                  #how many independent script you are hoping to run \n" %(len(K)*len(T)*len(A)*len(F)),
+"#SBATCH --ntasks-per-node=25 \n",
+"#SBATCH --time=18:00:00                         #compute time \n",
+"#SBATCH --mem=8gb                               #memory to request \n",
+"#SBATCH --output=./logs/%j.log                  #where to save output log files (julia script prints here) \n",
+"#SBATCH --error=./logs/%j.err                   #where to save output error files \n"
+]
 
-        for k in K:
-            for t in T:
-                for a in A: 
-                    for f in F:
-                            new.write("python clusterSweep.py %f %f %f %f" %(k, t, a, f))
+
+with open("slurmScript.sh","a") as new: 
+    for line in pre_schpeel:
+        new.write(line)
+
+    for k in K:
+        for t in T:
+            for a in A: 
+                for f in F:
+                    new.write("python clusterSweep.py %f %f %f %f &" %(k, t, a, f))
+                    new.write("\n")
--- otherScripts/slurmScript.sh
+++ otherScripts/slurmScript.sh
@@ -1,93 +1,90 @@
-#!/bin/bash
-
-#SBATCH --job-name=hpsweep   		#name of the job to find when calling >>>sacct or >>>squeue
-#SBATCH --nodes=4				#number of nodes, i.e. computers, to request off the cluster (nodes typically have ~20 singled threaded cores)
-#SBATCH --ntasks=100			#how many independent script you are hoping to run 
-#SBATCH --time=18:00:00				#compute time
-#SBATCH --mem=8gb				#memory to request
-#SBATCH --output=./logs/%j.log			#where to save output log files (julia script prints here)  
-#SBATCH --error=./logs/%j.err			#where to save output error files
-
-pwd; hostname; date
-
-python clusterSweep.py 0.500000 0.200000 0.600000 0.600000
-python clusterSweep.py 0.500000 0.200000 0.600000 0.800000
-python clusterSweep.py 0.500000 0.200000 0.600000 1.000000
-python clusterSweep.py 0.500000 0.200000 0.700000 0.600000
-python clusterSweep.py 0.500000 0.200000 0.700000 0.800000
-python clusterSweep.py 0.500000 0.200000 0.700000 1.000000
-python clusterSweep.py 0.500000 0.200000 0.800000 0.600000
-python clusterSweep.py 0.500000 0.200000 0.800000 0.800000
-python clusterSweep.py 0.500000 0.200000 0.800000 1.000000
-python clusterSweep.py 0.500000 0.025000 0.600000 0.600000
-python clusterSweep.py 0.500000 0.025000 0.600000 0.800000
-python clusterSweep.py 0.500000 0.025000 0.600000 1.000000
-python clusterSweep.py 0.500000 0.025000 0.700000 0.600000
-python clusterSweep.py 0.500000 0.025000 0.700000 0.800000
-python clusterSweep.py 0.500000 0.025000 0.700000 1.000000
-python clusterSweep.py 0.500000 0.025000 0.800000 0.600000
-python clusterSweep.py 0.500000 0.025000 0.800000 0.800000
-python clusterSweep.py 0.500000 0.025000 0.800000 1.000000
-python clusterSweep.py 0.500000 0.030000 0.600000 0.600000
-python clusterSweep.py 0.500000 0.030000 0.600000 0.800000
-python clusterSweep.py 0.500000 0.030000 0.600000 1.000000
-python clusterSweep.py 0.500000 0.030000 0.700000 0.600000
-python clusterSweep.py 0.500000 0.030000 0.700000 0.800000
-python clusterSweep.py 0.500000 0.030000 0.700000 1.000000
-python clusterSweep.py 0.500000 0.030000 0.800000 0.600000
-python clusterSweep.py 0.500000 0.030000 0.800000 0.800000
-python clusterSweep.py 0.500000 0.030000 0.800000 1.000000
-python clusterSweep.py 1.000000 0.200000 0.600000 0.600000
-python clusterSweep.py 1.000000 0.200000 0.600000 0.800000
-python clusterSweep.py 1.000000 0.200000 0.600000 1.000000
-python clusterSweep.py 1.000000 0.200000 0.700000 0.600000
-python clusterSweep.py 1.000000 0.200000 0.700000 0.800000
-python clusterSweep.py 1.000000 0.200000 0.700000 1.000000
-python clusterSweep.py 1.000000 0.200000 0.800000 0.600000
-python clusterSweep.py 1.000000 0.200000 0.800000 0.800000
-python clusterSweep.py 1.000000 0.200000 0.800000 1.000000
-python clusterSweep.py 1.000000 0.025000 0.600000 0.600000
-python clusterSweep.py 1.000000 0.025000 0.600000 0.800000
-python clusterSweep.py 1.000000 0.025000 0.600000 1.000000
-python clusterSweep.py 1.000000 0.025000 0.700000 0.600000
-python clusterSweep.py 1.000000 0.025000 0.700000 0.800000
-python clusterSweep.py 1.000000 0.025000 0.700000 1.000000
-python clusterSweep.py 1.000000 0.025000 0.800000 0.600000
-python clusterSweep.py 1.000000 0.025000 0.800000 0.800000
-python clusterSweep.py 1.000000 0.025000 0.800000 1.000000
-python clusterSweep.py 1.000000 0.030000 0.600000 0.600000
-python clusterSweep.py 1.000000 0.030000 0.600000 0.800000
-python clusterSweep.py 1.000000 0.030000 0.600000 1.000000
-python clusterSweep.py 1.000000 0.030000 0.700000 0.600000
-python clusterSweep.py 1.000000 0.030000 0.700000 0.800000
-python clusterSweep.py 1.000000 0.030000 0.700000 1.000000
-python clusterSweep.py 1.000000 0.030000 0.800000 0.600000
-python clusterSweep.py 1.000000 0.030000 0.800000 0.800000
-python clusterSweep.py 1.000000 0.030000 0.800000 1.000000
-python clusterSweep.py 2.000000 0.200000 0.600000 0.600000
-python clusterSweep.py 2.000000 0.200000 0.600000 0.800000
-python clusterSweep.py 2.000000 0.200000 0.600000 1.000000
-python clusterSweep.py 2.000000 0.200000 0.700000 0.600000
-python clusterSweep.py 2.000000 0.200000 0.700000 0.800000
-python clusterSweep.py 2.000000 0.200000 0.700000 1.000000
-python clusterSweep.py 2.000000 0.200000 0.800000 0.600000
-python clusterSweep.py 2.000000 0.200000 0.800000 0.800000
-python clusterSweep.py 2.000000 0.200000 0.800000 1.000000
-python clusterSweep.py 2.000000 0.025000 0.600000 0.600000
-python clusterSweep.py 2.000000 0.025000 0.600000 0.800000
-python clusterSweep.py 2.000000 0.025000 0.600000 1.000000
-python clusterSweep.py 2.000000 0.025000 0.700000 0.600000
-python clusterSweep.py 2.000000 0.025000 0.700000 0.800000
-python clusterSweep.py 2.000000 0.025000 0.700000 1.000000
-python clusterSweep.py 2.000000 0.025000 0.800000 0.600000
-python clusterSweep.py 2.000000 0.025000 0.800000 0.800000
-python clusterSweep.py 2.000000 0.025000 0.800000 1.000000
-python clusterSweep.py 2.000000 0.030000 0.600000 0.600000
-python clusterSweep.py 2.000000 0.030000 0.600000 0.800000
-python clusterSweep.py 2.000000 0.030000 0.600000 1.000000
-python clusterSweep.py 2.000000 0.030000 0.700000 0.600000
-python clusterSweep.py 2.000000 0.030000 0.700000 0.800000
-python clusterSweep.py 2.000000 0.030000 0.700000 1.000000
-python clusterSweep.py 2.000000 0.030000 0.800000 0.600000
-python clusterSweep.py 2.000000 0.030000 0.800000 0.800000
-python clusterSweep.py 2.000000 0.030000 0.800000 1.000000
+#!/bin/bash 
+#SBATCH --job-name=hpsweep              #name of the job to find when calling >>>sacct or >>>squeue 
+#SBATCH --nodes=4                               #number of nodes, i.e. computers, to request off the cluster (nodes typically have ~20 singled threaded cores) 
+#SBATCH --ntasks=81                  #how many independent script you are hoping to run 
+#SBATCH --ntasks-per-node=25 
+#SBATCH --time=18:00:00                         #compute time 
+#SBATCH --mem=8gb                               #memory to request 
+#SBATCH --output=./logs/%j.log                  #where to save output log files (julia script prints here) 
+#SBATCH --error=./logs/%j.err                   #where to save output error files 
+python clusterSweep.py 0.500000 0.200000 0.600000 0.600000 &
+python clusterSweep.py 0.500000 0.200000 0.600000 0.800000 &
+python clusterSweep.py 0.500000 0.200000 0.600000 1.000000 &
+python clusterSweep.py 0.500000 0.200000 0.700000 0.600000 &
+python clusterSweep.py 0.500000 0.200000 0.700000 0.800000 &
+python clusterSweep.py 0.500000 0.200000 0.700000 1.000000 &
+python clusterSweep.py 0.500000 0.200000 0.800000 0.600000 &
+python clusterSweep.py 0.500000 0.200000 0.800000 0.800000 &
+python clusterSweep.py 0.500000 0.200000 0.800000 1.000000 &
+python clusterSweep.py 0.500000 0.025000 0.600000 0.600000 &
+python clusterSweep.py 0.500000 0.025000 0.600000 0.800000 &
+python clusterSweep.py 0.500000 0.025000 0.600000 1.000000 &
+python clusterSweep.py 0.500000 0.025000 0.700000 0.600000 &
+python clusterSweep.py 0.500000 0.025000 0.700000 0.800000 &
+python clusterSweep.py 0.500000 0.025000 0.700000 1.000000 &
+python clusterSweep.py 0.500000 0.025000 0.800000 0.600000 &
+python clusterSweep.py 0.500000 0.025000 0.800000 0.800000 &
+python clusterSweep.py 0.500000 0.025000 0.800000 1.000000 &
+python clusterSweep.py 0.500000 0.030000 0.600000 0.600000 &
+python clusterSweep.py 0.500000 0.030000 0.600000 0.800000 &
+python clusterSweep.py 0.500000 0.030000 0.600000 1.000000 &
+python clusterSweep.py 0.500000 0.030000 0.700000 0.600000 &
+python clusterSweep.py 0.500000 0.030000 0.700000 0.800000 &
+python clusterSweep.py 0.500000 0.030000 0.700000 1.000000 &
+python clusterSweep.py 0.500000 0.030000 0.800000 0.600000 &
+python clusterSweep.py 0.500000 0.030000 0.800000 0.800000 &
+python clusterSweep.py 0.500000 0.030000 0.800000 1.000000 &
+python clusterSweep.py 1.000000 0.200000 0.600000 0.600000 &
+python clusterSweep.py 1.000000 0.200000 0.600000 0.800000 &
+python clusterSweep.py 1.000000 0.200000 0.600000 1.000000 &
+python clusterSweep.py 1.000000 0.200000 0.700000 0.600000 &
+python clusterSweep.py 1.000000 0.200000 0.700000 0.800000 &
+python clusterSweep.py 1.000000 0.200000 0.700000 1.000000 &
+python clusterSweep.py 1.000000 0.200000 0.800000 0.600000 &
+python clusterSweep.py 1.000000 0.200000 0.800000 0.800000 &
+python clusterSweep.py 1.000000 0.200000 0.800000 1.000000 &
+python clusterSweep.py 1.000000 0.025000 0.600000 0.600000 &
+python clusterSweep.py 1.000000 0.025000 0.600000 0.800000 &
+python clusterSweep.py 1.000000 0.025000 0.600000 1.000000 &
+python clusterSweep.py 1.000000 0.025000 0.700000 0.600000 &
+python clusterSweep.py 1.000000 0.025000 0.700000 0.800000 &
+python clusterSweep.py 1.000000 0.025000 0.700000 1.000000 &
+python clusterSweep.py 1.000000 0.025000 0.800000 0.600000 &
+python clusterSweep.py 1.000000 0.025000 0.800000 0.800000 &
+python clusterSweep.py 1.000000 0.025000 0.800000 1.000000 &
+python clusterSweep.py 1.000000 0.030000 0.600000 0.600000 &
+python clusterSweep.py 1.000000 0.030000 0.600000 0.800000 &
+python clusterSweep.py 1.000000 0.030000 0.600000 1.000000 &
+python clusterSweep.py 1.000000 0.030000 0.700000 0.600000 &
+python clusterSweep.py 1.000000 0.030000 0.700000 0.800000 &
+python clusterSweep.py 1.000000 0.030000 0.700000 1.000000 &
+python clusterSweep.py 1.000000 0.030000 0.800000 0.600000 &
+python clusterSweep.py 1.000000 0.030000 0.800000 0.800000 &
+python clusterSweep.py 1.000000 0.030000 0.800000 1.000000 &
+python clusterSweep.py 2.000000 0.200000 0.600000 0.600000 &
+python clusterSweep.py 2.000000 0.200000 0.600000 0.800000 &
+python clusterSweep.py 2.000000 0.200000 0.600000 1.000000 &
+python clusterSweep.py 2.000000 0.200000 0.700000 0.600000 &
+python clusterSweep.py 2.000000 0.200000 0.700000 0.800000 &
+python clusterSweep.py 2.000000 0.200000 0.700000 1.000000 &
+python clusterSweep.py 2.000000 0.200000 0.800000 0.600000 &
+python clusterSweep.py 2.000000 0.200000 0.800000 0.800000 &
+python clusterSweep.py 2.000000 0.200000 0.800000 1.000000 &
+python clusterSweep.py 2.000000 0.025000 0.600000 0.600000 &
+python clusterSweep.py 2.000000 0.025000 0.600000 0.800000 &
+python clusterSweep.py 2.000000 0.025000 0.600000 1.000000 &
+python clusterSweep.py 2.000000 0.025000 0.700000 0.600000 &
+python clusterSweep.py 2.000000 0.025000 0.700000 0.800000 &
+python clusterSweep.py 2.000000 0.025000 0.700000 1.000000 &
+python clusterSweep.py 2.000000 0.025000 0.800000 0.600000 &
+python clusterSweep.py 2.000000 0.025000 0.800000 0.800000 &
+python clusterSweep.py 2.000000 0.025000 0.800000 1.000000 &
+python clusterSweep.py 2.000000 0.030000 0.600000 0.600000 &
+python clusterSweep.py 2.000000 0.030000 0.600000 0.800000 &
+python clusterSweep.py 2.000000 0.030000 0.600000 1.000000 &
+python clusterSweep.py 2.000000 0.030000 0.700000 0.600000 &
+python clusterSweep.py 2.000000 0.030000 0.700000 0.800000 &
+python clusterSweep.py 2.000000 0.030000 0.700000 1.000000 &
+python clusterSweep.py 2.000000 0.030000 0.800000 0.600000 &
+python clusterSweep.py 2.000000 0.030000 0.800000 0.800000 &
+python clusterSweep.py 2.000000 0.030000 0.800000 1.000000 &
