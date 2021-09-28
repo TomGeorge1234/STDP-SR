2db9a07fb207056f07598811ceeaae2d5eb27358
Tom George <tgeorge@sgw2.id.swc.ucl.ac.uk>
2021-09-28 22:51:13 +0000

    figs

--- otherScripts/hpsweep/slurmMaker.py
+++ otherScripts/hpsweep/slurmMaker.py
@@ -2,11 +2,11 @@ import subprocess
 import numpy as np 
 subprocess.run("rm slurmScript.sh", shell=True)
 
-K = [0.01,0.05,0.1,0.5,1]
-T_STDP = [20e-3,25e-3, 30e-3]
+K = [0.01,0.1,0.5,1]
+T_STDP = [25e-3, 30e-3]
 T_SR = [3,4,5]
-A = [0.9,0.95,0.99]
-F = [0.5,0.6,0.7]
+A = [0.8,0.9,0.95]
+F = [0.5,0.75,1]
 
 #K = [1]
 #T_STDP = [20e-3]
