1b0cdfcb05da074c3f353f052dd4ad9d5733c011
Tom George <tomgeorge1@btinternet.com>
2021-10-11 19:09:19 +0100

    cluster sweep

--- otherScripts/hpsweep/clusterSweep.py
+++ otherScripts/hpsweep/clusterSweep.py
@@ -38,20 +38,20 @@ if type(fr) != list:
 
 
 
-for t_sr in t_sr:
-    for t_stdp in t_stdp:
-        for t_stdp_asymm in t_stdp_asymm:
-            for a_stdp_asymm in a_stdp_asymm:
-                for f in f:
-                    for k in k:
-                        for fr in fr:
-                            print(t_sr)
-                            print(t_stdp)
-                            print(t_stdp_asymm)
-                            print(a_stdp_asymm)
-                            print(f)
-                            print(k)
-                            print(fr)
+for t_sr_ in t_sr:
+    for t_stdp_ in t_stdp:
+        for t_stdp_asymm_ in t_stdp_asymm:
+            for a_stdp_asymm_ in a_stdp_asymm:
+                for f_ in f:
+                    for k_ in k:
+                        for fr_ in fr:
+                            # print(t_sr)
+                            # print(t_stdp)
+                            # print(t_stdp_asymm)
+                            # print(a_stdp_asymm)
+                            # print(f)
+                            # print(k)
+                            # print(fr)
                             params = { 
 
                                 #Maze params 
@@ -67,17 +67,17 @@ for t_sr in t_sr:
                                 'doorsClosed'         : False,       #whether doors are opened or closed in multicompartment maze
 
                                 #TD params 
-                                'tau'                 : t_sr,          #TD decay time, seconds
-                                'peakFiringRate'      : fr,          #peak firing rate of a cell (middle of place field, preferred theta phase)
+                                'tau'                 : t_sr_,          #TD decay time, seconds
+                                'peakFiringRate'      : fr_,          #peak firing rate of a cell (middle of place field, preferred theta phase)
                                 
                                 #STDP params
-                                'tau_STDP'            : t_stdp,      #rate trace decays
-                                'tau_STDP_asymm'      : t_stdp_asymm,          # tau- = this * tau+ 
-                                'a_STDP_asymm'        : a_stdp_asymm,       #post-before-pre potentiation factor = this * pre-before-post
+                                'tau_STDP'            : t_stdp_,      #rate trace decays
+                                'tau_STDP_asymm'      : t_stdp_asymm_,          # tau- = this * tau+ 
+                                'a_STDP_asymm'        : a_stdp_asymm_,       #post-before-pre potentiation factor = this * pre-before-post
 
                                 #Theta precession params
-                                'precessFraction'     : f,        #fraction of 2pi the prefered phase moves through
-                                'kappa'               : k,          # von mises spread parameter
+                                'precessFraction'     : f_,        #fraction of 2pi the prefered phase moves through
+                                'kappa'               : k_,          # von mises spread parameter
                                 }
 
                             agent =  MazeAgent(params)
@@ -85,14 +85,14 @@ for t_sr in t_sr:
                             plotter = Visualiser(agent)
 
                             fig, ax = plotter.plotMAveraged(time=30)
-                            saveFigure(fig,'Mav',specialLocation='../../figures/clusterSweep/Mav_%g_%g_%g_%g_%g_%g.svg' %(int(1000*t_sr),int(1000*t_stdp),int(1000*t_stdp_asymm),int(1000*a_stdp_asymm),int(1000*f),int(1000*k)),figureDirectory="../../figures/")
+                            saveFigure(fig,'Mav',specialLocation='../../figures/clusterSweep/Mav_%g_%g_%g_%g_%g_%g.svg' %(int(1000*t_sr_),int(1000*t_stdp_),int(1000*t_stdp_asymm_),int(1000*a_stdp_asymm_),int(1000*f_),int(1000*k_)),figureDirectory="../../figures/")
 
                             fig1, ax1 = plotter.plotVarianceAndError()
-                            saveFigure(fig1,'Mvar',specialLocation='../../figures/clusterSweep/Mvar_%g_%g_%g_%g_%g_%g.svg' %(int(1000*t_sr),int(1000*t_stdp),int(1000*t_stdp_asymm),int(1000*a_stdp_asymm),int(1000*f),int(1000*k)),figureDirectory="../../figures/")
+                            saveFigure(fig1,'Mvar',specialLocation='../../figures/clusterSweep/Mvar_%g_%g_%g_%g_%g_%g.svg' %(int(1000*t_sr_),int(1000*t_stdp_),int(1000*t_stdp_asymm_),int(1000*a_stdp_asymm_),int(1000*f_),int(1000*k_)),figureDirectory="../../figures/")
                             
                             R_w, R_nw, SNR_w, SNR_nw = agent.getMetrics(time=30)
 
-                            data = [str(t_sr),str(t_stdp),str(t_stdp_asymm),str(a_stdp_asymm), str(f), str(k), str(fr), str(round(R_w,5)), str(round(R_nw,5)), str(round(SNR_w,5)), str(round(SNR_nw,5))]
+                            data = [str(t_sr_),str(t_stdp_),str(t_stdp_asymm_),str(a_stdp_asymm_), str(f_), str(k_), str(fr_), str(round(R_w,5)), str(round(R_nw,5)), str(round(SNR_w,5)), str(round(SNR_nw,5))]
                             with open("sweepResults.txt", "a") as file: 
                                 if sum(1 for line in open('sweepResults.txt')) == 0:
                                     file.write("t_sr,t_stdp,t_stdp_asymm,a_stdp_asymm,f,k,fr,R_w,R_nw,SNR_w,SNR_nw")
